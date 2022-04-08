adfeef374c469bf2bebf21607181a2a9df1b892a
Stanislav Kosáček <stanislav.kosacek@icloud.com>
2022-04-08 16:50:53 +0200

    fix removed description, add originalTitle, remove some properties from list endpoint

--- pages/api/movies/index.ts
+++ pages/api/movies/index.ts
@@ -6,30 +6,40 @@ import movies from './movies.json';
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const genre = req.query.genre ? String(req.query.genre) : null;
   const year = req.query.year ? Number(req.query.year) : null;
-  simplifyData(movies);
+  const moviesSimplifyed: Array<any> = simplifyData(movies);
 
   if (genre && year) {
-    const filtered = movies.filter((movie) => {
+    const filtered = moviesSimplifyed.filter((movie) => {
       return movie.genres.includes(genre.toLowerCase()) && movie.year === year;
     });
     res.status(200).json(filtered);
   } else if (genre) {
-    const filtered = movies.filter((movie) => {
+    const filtered = moviesSimplifyed.filter((movie) => {
       return movie.genres.includes(genre.toLowerCase());
     });
     res.status(200).json(filtered);
   } else if (year) {
-    const filtered = movies.filter((movie) => {
+    const filtered = moviesSimplifyed.filter((movie) => {
       return movie.year === year;
     });
     res.status(200).json(filtered);
   } else {
-    res.status(200).json(movies);
+    res.status(200).json(moviesSimplifyed);
   }
 }
 
 const simplifyData = (movies: Array<any>) => {
+  const moviesSimplifyed: Array<any> = [];
   movies.forEach((movie: any) => {
-    delete movie.description;
+    moviesSimplifyed.push({
+      id: movie.id,
+      title: movie.title,
+      url: movie.url,
+      posterUrl: movie.posterUrl,
+      genres: movie.genres,
+      year: movie.year,
+    });
   });
+
+  return moviesSimplifyed;
 };
